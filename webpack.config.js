15e46157460c21f000cf41d476d00349711e6287
zayn-m <zain.hassan1212@gmail.com>
2021-08-09 21:51:59 +0500

    webpack configured

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,32 @@
-const path = require('path');
-
+const path = require('path'); // <-get absolute location for saving
+const pkg = require('./package.json');
+const nodeExternals = require('webpack-node-externals');
 module.exports = {
-  mode: 'production',
-  entry: './src/index.js',
+  entry: "./src/index.js", // <- starting point for bundle 
   output: {
-    path: path.resolve('dist'),
-    filename: 'index.js',
-    libraryTarget: 'commonjs2',
+    path: path.resolve(__dirname, 'dist'), //<-where to save ur bundle 
+    filename: "index.js", //<-filename for bundled file
+    library: pkg.name,
+    libraryTarget: "commonjs2" //<- to which version are we compiling js 
   },
   module: {
     rules: [
       {
-        test: /\.js?$/,
-        exclude: /(node_modules)/,
-        use: 'babel-loader',
-      },
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+ },
       {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      }
-    ],
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
+ }
+ ]
   },
-  resolve: {
-    alias: {
-      'react': path.resolve(__dirname, './node_modules/react'),
-      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
-    }
-  },
-  externals: {
-    // Don't bundle react or react-dom      
-    react: {
-      commonjs: "react",
-      commonjs2: "react",
-      amd: "React",
-      root: "React"
-    },
-    "react-dom": {
-      commonjs: "react-dom",
-      commonjs2: "react-dom",
-      amd: "ReactDOM",
-      root: "ReactDOM"
-    }
-  }
+  target: 'node',
+  externals: [nodeExternals()]
+  // if we need to bundle anything extra with our code, if nothing entered means nothing else to compile, else we can mention, if, in case we want to bundle a particular third party code with our codebase
+  // if we need to bundle anything extra with our code (3rd party package),we can add it as an argument to this method.
+  // else, if nothing is passed as argument means no additional dependency to compile.
 };
